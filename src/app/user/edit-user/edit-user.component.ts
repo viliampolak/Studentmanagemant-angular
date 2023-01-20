f1cfcfeef37f886965e0477f9b70a7dab5e8e5e8
viliampolak <91248882+viliampolak@users.noreply.github.com>
2023-01-21 00:40:27 +0100

    2 files modified

--- src/app/user/edit-user/edit-user.component.ts
+++ src/app/user/edit-user/edit-user.component.ts
@@ -1,21 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../../user.service';
 
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit-user.component.html',
-  styleUrls: ['./edit-user.component.css']
+  styleUrls: ['./edit-user.component.css'],
 })
 export class EditUserComponent implements OnInit {
-
+  studentForm: FormGroup;
   constructor(
+    private formBuilder: FormBuilder,
     private router: Router,
     private studentService: UserService,
     private route: ActivatedRoute
-  ) { }
+  ) {}
 
   ngOnInit() {
+    this.studentForm = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      class: [''],
+      age: [''],
+      birthdate: [''],
+      specia: [''],
+      gender: [''],
+      average: [''],
+      disabled: [],
+      awards: [''],
+    });
   }
 
+  submit() {
+    if (this.studentForm.valid) {
+      let data: [{}] = JSON.parse(localStorage.getItem('students') || '[]');
+      localStorage.setItem(
+        'students',
+        JSON.stringify([
+          ...data,
+          {
+            ...this.studentForm.value,
+            lastedit: new Date(),
+          },
+        ])
+      );
+      this.router.navigate(['listuser']);
+    }
+  }
 }
