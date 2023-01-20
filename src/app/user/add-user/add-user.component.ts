95f7ed22f7a27691770d2a2483833fb076c5970e
viliampolak <91248882+viliampolak@users.noreply.github.com>
2023-01-20 23:17:11 +0100

    2 files modified, 1 deleted

--- src/app/user/add-user/add-user.component.ts
+++ src/app/user/add-user/add-user.component.ts
@@ -5,7 +5,7 @@ import { UserService } from '../../user.service';
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
-  styleUrls: ['./add-user.component.css']
+  styleUrls: ['./add-user.component.css'],
 })
 export class AddUserComponent implements OnInit {
 
@@ -16,6 +16,45 @@ export class AddUserComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    
+    if (localStorage.getItem('logged') != 'neznama') {
+      this.router.navigate(['./']);
+      }
   }
+  submit(
+    name: string,
+    lastname: string,
+    classko: string,
+    age: string,
+    date: string,
+    spec: string,
+    gender: string,
+    average: string,
+    disabled: string,
+    awards: string
+  
+  ): void {
+    let data: [{}] = JSON.parse(localStorage.getItem('students') || '[{}]');
+    localStorage.setItem(
+      'students',
+      JSON.stringify([
+        ...data,
+      {
+          name: name,
+          lastname: lastname,
+          classko: classko,
+          age: age,
+          date: date,
+          spec: spec,
+          gender: gender,
+          average: average,
+          disabled: disabled,
+          awards: awards,
+          lastedit: new Date()
+      }
+      ])
+    );
 
+    this.router.navigate(['listuser']);
+  }
 }
