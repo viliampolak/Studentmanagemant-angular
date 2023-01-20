32154d8f19ff9b58b41b91d296403641104913e8
viliampolak <91248882+viliampolak@users.noreply.github.com>
2023-01-21 00:43:19 +0100

    1 file modified

--- src/app/user/edit-user/edit-user.component.ts
+++ src/app/user/edit-user/edit-user.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { UserService } from '../../user.service';
 
 @Component({
   selector: 'app-edit-user',
@@ -12,40 +11,58 @@ export class EditUserComponent implements OnInit {
   studentForm: FormGroup;
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router,
-    private studentService: UserService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
+  student: any;
+
   ngOnInit() {
+    if (localStorage.getItem('admin') != 't') {
+      this.router.navigate(['login']);
+    }
+    this.student = JSON.parse(localStorage.getItem('detail') || '{}');
     this.studentForm = this.formBuilder.group({
       firstname: ['', Validators.required],
       lastname: ['', Validators.required],
       class: [''],
       age: [''],
-      birthdate: [''],
-      specia: [''],
+      date: [''],
+      spec: [''],
       gender: [''],
       average: [''],
       disabled: [],
       awards: [''],
     });
+    this.studentForm.get('name').setValue(this.student.firstname);
+    this.studentForm.get('lastname').setValue(this.student.lastname);
+    this.studentForm.get('class').setValue(this.student.class);
+    this.studentForm.get('age').setValue(this.student.age);
+    this.studentForm.get('date').setValue(this.student.date);
+    this.studentForm.get('spec').setValue(this.student.spec);
+    this.studentForm.get('gender').setValue(this.student.gender);
+    this.studentForm.get('average').setValue(this.student.average);
+    this.studentForm.get('disabled').setValue(this.student.disabled);
+    this.studentForm.get('awards').setValue(this.student.awards);
   }
-
   submit() {
     if (this.studentForm.valid) {
-      let data: [{}] = JSON.parse(localStorage.getItem('students') || '[]');
       localStorage.setItem(
         'students',
-        JSON.stringify([
-          ...data,
-          {
-            ...this.studentForm.value,
-            lastedit: new Date(),
-          },
-        ])
+        JSON.stringify(
+          JSON.parse(localStorage.getItem('students') || '[]').map(
+            (studentt: any) =>
+              studentt.lastedit == this.student.lastedit
+                ? {
+                    ...studentt,
+                    lastedit: new Date(),
+                    ...this.studentForm.value,
+                  }
+                : studentt
+          )
+        )
       );
-      this.router.navigate(['listuser']);
+      this.router.navigate(['home']);
     }
   }
 }
